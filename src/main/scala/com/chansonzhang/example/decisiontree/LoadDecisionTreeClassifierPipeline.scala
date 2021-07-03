1c045c874611d966ad5eef9d00ec2e8314e350f3
zhangchen <zhangchen.shaanxi@gmail.com>
2021-07-03 21:03:19 +0800

    add decision tree classifier example

---
+++ src/main/scala/com/chansonzhang/example/decisiontree/LoadDecisionTreeClassifierPipeline.scala
@@ -0,0 +1,65 @@
+package com.chansonzhang.example.decisiontree
+
+/**
+ * Copyright 2021 Zhang, Chen. All Rights Reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @since 2021/5/31 17:07
+ * @author Zhang Chen(ChansonZhang)
+ */
+
+import ml.combust.bundle.BundleFile
+import ml.combust.mleap.core.types._
+import ml.combust.mleap.runtime.MleapContext
+import ml.combust.mleap.runtime.MleapSupport._
+import ml.combust.mleap.runtime.frame.{DefaultLeapFrame, Row}
+import resource._
+import org.apache.spark.ml.bundle.SparkBundleContext
+import breeze.storage.Zero
+import ml.dmlc.xgboost4j.scala.spark.mleap.XGBoostClassificationModelOp
+
+object LoadDecisionTreeClassifierPipeline {
+  implicit val sbc: MleapContext = MleapContext()
+
+  def main(args: Array[String]): Unit = {
+
+    // load the Spark pipeline we saved in the previous section
+    val bundle = (for (bundleFile <- managed(BundleFile("jar:file:/Users/zhangchen/code/mleap-examples/tmp/dt-pipeline.zip")))
+      yield {
+        bundleFile.loadMleapBundle().get
+      }).opt.get
+
+    // create a simple LeapFrame to transform
+    // MLeap makes extensive use of monadic types like Try
+    val schema = StructType(
+      StructField("sepallength", ScalarType.Double),
+      StructField("sepalwidth", ScalarType.Double),
+      StructField("petallength", ScalarType.Double),
+      StructField("petalwidth", ScalarType.Double)).get
+    //5.1,3.5,1.4,.2,"Setosa"
+    //6.9,3.1,4.9,1.5,"Versicolor"
+    //5.7,2.5,5,2,"Virginica"
+    val data = Seq(
+      Row(5.1, 3.5, 1.4, .2),
+      Row(6.9, 3.1, 4.9, 1.5),
+      Row(5.7, 2.5, 5.0, 2.0))
+    val frame = DefaultLeapFrame(schema, data)
+
+    // transform the dataframe using our pipeline
+    val mleapPipeline = bundle.root
+    val frame2 = mleapPipeline.transform(frame).get
+    frame2.show()
+
+  }
+}
---
+++ src/main/scala/com/chansonzhang/example/decisiontree/spark/DecisionTreeClassifierSparkOperator.scala
@@ -0,0 +1,101 @@
+package com.chansonzhang.example.decisiontree.spark
+
+
+/**
+ * Copyright 2021 Zhang, Chen. All Rights Reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @since 2021/6/20 15:26
+ * @author Zhang Chen(ChansonZhang)
+ */
+
+import ml.combust.bundle.BundleFile
+import ml.combust.mleap.spark.SparkSupport.SparkTransformerOps
+import ml.dmlc.xgboost4j.scala.spark.XGBoostClassifier
+import org.apache.spark.ml.bundle.SparkBundleContext
+import org.apache.spark.ml.classification.DecisionTreeClassifier
+import org.apache.spark.ml.{Pipeline, PipelineModel}
+import org.apache.spark.ml.feature.{IndexToString, StringIndexer, StringIndexerModel, VectorAssembler, VectorIndexer}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import resource.managed
+
+object DecisionTreeClassifierSparkOperator {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .master("local[3]")
+      .getOrCreate()
+    val irisDf = spark.read
+      .option("header", value = true)
+      .option("inferSchema", value = true)
+      .csv("src/main/resources/iris.csv")
+    println("data length: " + irisDf.count())
+    irisDf.show(false)
+    val stringIndexerModel = new StringIndexer()
+      .setInputCol("variety")
+      .setOutputCol("varietyIndex")
+      .fit(irisDf)
+
+    val Array(train, test) = stringIndexerModel.transform(irisDf)
+      .randomSplit(Array(0.8, 0.2), 123456)
+    println("train length: " + train.count())
+    println("test length: " + test.count())
+
+    val dtPipeline = operator(
+      train,
+      Array("sepallength", "sepalwidth", "petallength", "petalwidth"),
+      labelCol = "varietyIndex",
+      stringIndexerModel
+    )
+
+    val predict = dtPipeline.transform(test)
+    predict.show(false)
+
+    // then serialize pipeline
+    val sbc = SparkBundleContext().withDataset(predict)
+    for (bf <- managed(BundleFile("jar:file:/Users/zhangchen/code/mleap-examples/tmp/dt-pipeline.zip"))) {
+      dtPipeline.writeBundle.save(bf)(sbc).get
+    }
+  }
+
+  def operator(df: DataFrame,
+               inputCols: Array[String],
+               labelCol: String,
+               stringIndexerModel: StringIndexerModel
+              ): PipelineModel = {
+
+    val vectorAssembler = new VectorAssembler().
+      setInputCols(inputCols).
+      setOutputCol("features")
+
+    // Automatically identify categorical features, and index them.
+    val featureIndexer = new VectorIndexer()
+      .setInputCol("features")
+      .setOutputCol("indexedFeatures")
+      .setMaxCategories(3) // features with > 4 distinct values are treated as continuous.
+
+    val dt = new DecisionTreeClassifier()
+      .setFeaturesCol("indexedFeatures").
+      setLabelCol(labelCol)
+
+    val labelConvertor = new IndexToString()
+      .setLabels(stringIndexerModel.labels)
+      .setInputCol("prediction")
+      .setOutputCol("predictVariety")
+
+    val pipeline = new Pipeline()
+      .setStages(Array(vectorAssembler, featureIndexer, dt, labelConvertor))
+
+    pipeline.fit(df)
+  }
+}
